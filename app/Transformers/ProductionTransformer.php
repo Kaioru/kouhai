6e58da6c13735910859cd2ec3b718a92b1c8ddc8
Kaioru <keifuurawrs@gmail.com>
2017-02-24 23:30:23 +0800

    Add date data to transformers

--- app/Transformers/ProductionTransformer.php
+++ app/Transformers/ProductionTransformer.php
@@ -16,6 +16,8 @@ class ProductionTransformer extends TransformerAbstract
 			'description' => (string) $model->description,
 			'creator_id' => (int) $model->creator_id,
 			'updater_id' => (int) $model->updater_id,
+			'created_at' => (string) $model->created_at,
+			'updated_at' => (string) $model->updated_at,
         ];
     }
 }
